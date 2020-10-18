8c28c111d8a06c5311a38b845fe2aa416d2b8fbe
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2020-10-18 18:44:29 -0300

    refactoring

--- tests/dotnetProcessingManualTest/RandomWalkerSketch.cs
+++ tests/dotnetProcessingManualTest/RandomWalkerSketch.cs
@@ -7,6 +7,7 @@ namespace dotnetProcessingManualTest
         private int x = 0;
         private int y = 0;
         private int internal_radius = 10;
+
         private void drawRandomWalker()
         {
             stroke(255, 100);
@@ -17,16 +18,16 @@ namespace dotnetProcessingManualTest
             switch (r)
             {
                 case 0:
-                    x = x + internal_radius;
+                    x += internal_radius;
                     break;
                 case 1:
-                    x = x - internal_radius;
+                    x -= internal_radius;
                     break;
                 case 2:
-                    y = y + internal_radius;
+                    y += internal_radius;
                     break;
                 case 3:
-                    y = y - internal_radius;
+                    y -= internal_radius;
                     break;
                      
             }
@@ -39,8 +40,9 @@ namespace dotnetProcessingManualTest
         public override void Setup()
         {
             size(600, 600);
-            translate(width / 2, height / 2);
             background(0);
+            translate(width / 2, height / 2);
+            
             stroke(255, 0, 255);
             strokeWeight(internal_radius);
             point(x, y);
